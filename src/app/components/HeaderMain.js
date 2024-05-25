2cecc4741d4e93e5314c96e80448a2235e23cdb3
RabinaSuwal <106600261+RabinaSuwal@users.noreply.github.com>
2024-05-25 20:27:25 +0545

    almost completed header section

--- src/app/components/HeaderMain.js
+++ src/app/components/HeaderMain.js
@@ -1,3 +1,5 @@
+import { FaRegHeart, FaSearch, FaShoppingBag } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa6";
 
 export default function HeaderMain() {
   return (
@@ -8,7 +10,30 @@ export default function HeaderMain() {
             <div className="w-full sm:w-[300px] md:w-[70%] relative">
                 <input className="border-gray-300 border p-2 px-4 rounded-lg w-full" 
                 type="text" placeholder="Enter any product name..." />
+            <FaSearch className="absolute right-0 top-0 mr-3 mt-3 "/>
             </div>
+            
+            <div className="hidden lg:flex gap-4 text-[30px]">
+             <div>
+                <FaRegUser/>
+              </div>
+
+              <div className="relative">
+                <FaRegHeart/>
+                <div className="bg-red-500 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[12px]
+                 text-white grid place-items-center translate-x-1 translate-y-1">
+                </div>
+              </div>
+
+              <div className="relative">
+              <FaShoppingBag/>
+              <div className="bg-red-500 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[12px]
+                 text-white grid place-items-center translate-x-1 translate-y-1">
+                </div>
+              </div>
+            </div>
+              
+            
 
         </div>   
     </div>
