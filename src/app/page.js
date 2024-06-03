2f6c8d6354e423e1bb35a21da8bbb6776c4b0f00
RabinaSuwal <106600261+RabinaSuwal@users.noreply.github.com>
2024-06-03 23:17:23 +0545

    working on images

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,13 @@
 export default function Home() {
   return (
     <div>
-      
+      <img className="container w-[100%] "
+      src="https://images.pexels.com/photos/974911/pexels-photo-974911.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+      alt="" />
+
+      <img className="container"
+      src="https://images.pexels.com/photos/972884/pexels-photo-972884.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+      alt="" />
     </div>
   );
 }
